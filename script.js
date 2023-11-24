b7fbfae3a219446875a122b157f50ef7080eb1f8
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2023-11-24 18:36:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 // complete this js code
 function Person(name, age) {
-	constructor(name, age){
+	
 		this.name=name;
 		this.age=age;
-	}
+	
 }
 Person.prototype.greet=function () {
 	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
 }
 
 function Employee(name, age, jobTitle) {
-	constructor(name, age, jobTitle){
+	
 		this.name=name;
 		this.age=age;
 		this.jobTitle=jobTitle;
-	}
+	
 }
 
 Employee.prototype.__proto__=Person.prototype;
 Employee.prototype.jobGreet=function () {
 	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 }
+
 
 // Do not change code below this line
 window.Person = Person;
